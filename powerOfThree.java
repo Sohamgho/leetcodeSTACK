2c7ca813620d83554bf404b04a6aed0155eedadd
Sohamgho <135148448+Sohamgho@users.noreply.github.com>
2025-01-08 11:20:53 +0530

    326. Power of Three
    link --> https://leetcode.com/problems/power-of-three/?envType=problem-list-v2&envId=math

---
+++ powerOfThree.java
@@ -0,0 +1,14 @@
+/*
+link --> https://leetcode.com/problems/power-of-three/?envType=problem-list-v2&envId=math
+*/
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) {
+            return false; 
+        }
+        while (n % 3 == 0) {
+            n = n / 3;
+        }
+        return n == 1; 
+    }
+}
